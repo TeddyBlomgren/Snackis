b071895ba9a400d32bcab18488d5e3f1a0e5ce38
Teddy Blomgren <Teddy_95@live.se>
2025-05-29 16:19:25 +0200

    Added 5 latest post on main

--- Snackis/Models/Comment.cs
+++ Snackis/Models/Comment.cs
@@ -1,8 +1,13 @@
-﻿namespace Snackis.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Snackis.Models
+
 {
     public class Comment
     {
         public int id { get; set; }
+        [Required(ErrorMessage = "Kommentaren får inte vara tom")]
         public string Text { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
         public string UserId { get; set; }
--- Snackis/Pages/Index.cshtml.cs
+++ Snackis/Pages/Index.cshtml.cs
@@ -1,28 +1,43 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Snackis.Data;
+using Snackis.Models;
 
 namespace Snackis.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly UserManager<SnackisUser> _userManager;
-        public IndexModel(UserManager<SnackisUser> userManager)
+        private readonly ForumDbContext _context;
+
+        public IndexModel(UserManager<SnackisUser> userManager, ForumDbContext context)
         {
             _userManager = userManager;
+            _context = context;
         }
+
         public string? DisplayName { get; set; }
+        public Dictionary<string, List<Post>> LatestPostsByCategory { get; set; }
+
+        public List<Post> LatestPosts { get; set; }
+
         public async Task OnGetAsync()
         {
             if (User.Identity.IsAuthenticated)
             {
                 var user = await _userManager.GetUserAsync(User);
-
                 DisplayName = user?.DisplayName;
-
             }
 
+            LatestPosts = await _context.Posts
+                .OrderByDescending(p => p.Date)
+                .Take(5)
+                .Include(p => p.User)
+                .ToListAsync();
         }
+
+
     }
 }
