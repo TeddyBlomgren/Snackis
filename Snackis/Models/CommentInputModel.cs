4671d4da69c0055c9371446f9391caa622e5cc36
Teddy Blomgren <Teddy_95@live.se>
2025-05-29 16:45:25 +0200

    Fixed comments

---
+++ Snackis/Models/CommentInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Snackis.Models
+{
+    public class CommentInputModel
+    {
+        [Required(ErrorMessage = "Kommentaren får inte vara tom")]
+        public string Text { get; set; }
+    }
+}
+
--- Snackis/Pages/Kategorier/PostDetails.cshtml.cs
+++ Snackis/Pages/Kategorier/PostDetails.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Snackis.Data;
 using Snackis.Models;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ namespace Snackis.Pages.Kategorier
         public Post Post { get; set; }
 
         [BindProperty]
-        public Comment NewComment { get; set; }
+        public CommentInputModel Input { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
@@ -42,32 +43,51 @@ namespace Snackis.Pages.Kategorier
 
         public async Task<IActionResult> OnPostAsync(int id)
         {
+            Console.WriteLine("DEBUG: OnPostAsync startar, id = " + id);
+
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
+                Console.WriteLine("DEBUG: Ingen användare hittades, Unauthorized.");
                 return Unauthorized();
             }
 
             var post = await _context.Posts.Include(p => p.Comments).FirstOrDefaultAsync(p => p.Id == id);
             if (post == null)
             {
+                Console.WriteLine("DEBUG: Inget inlägg hittades med id = " + id);
                 return NotFound();
             }
 
+            Console.WriteLine("DEBUG: ModelState.IsValid = " + ModelState.IsValid);
             if (!ModelState.IsValid)
             {
+                foreach (var kvp in ModelState)
+                {
+                    foreach (var error in kvp.Value.Errors)
+                    {
+                        Console.WriteLine($"DEBUG: ModelState-fel i {kvp.Key}: {error.ErrorMessage}");
+                    }
+                }
+
                 Post = post;
                 return Page();
             }
 
-            NewComment.PostId = id;
-            NewComment.UserId = user.Id;
-            NewComment.DisplayName = user.DisplayName;
-            NewComment.Date = System.DateTime.Now;
+            var newComment = new Comment
+            {
+                PostId = id,
+                UserId = user.Id,
+                UserName = user.UserName,
+                DisplayName = user.DisplayName,
+                Date = DateTime.Now,
+                Text = Input.Text
+            };
 
-            _context.Comments.Add(NewComment);
+            _context.Comments.Add(newComment);
             await _context.SaveChangesAsync();
 
+            Console.WriteLine("DEBUG: Kommentar sparad och redirect körs.");
             return RedirectToPage("./PostDetails", new { id = id });
         }
     }
